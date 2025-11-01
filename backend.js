e819a0a6b116c14963e2f2c634be347352e93625
Drydar <d.rydar001@gmail.com>
2025-11-01 00:55:40 -0800

    Add files via upload

--- backend.js
+++ backend.js
@@ -322,7 +322,7 @@ if (bankForm) {
         email: user.email,
         type: "bank",
         amount: parseFloat(amount),
-        details: { bankName, accountNumber },
+        details: { bankName, accountNumber, accountName },
         status: "pending",
         date: serverTimestamp(),
       });
